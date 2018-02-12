201e7ddb4c49c753cd291f04501a28f0cbc08d1c
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2018-02-12 21:38:40 +0000

    Update interface
    includes find adapters and get subnet ip

--- yArp/yArp/Form1.Designer.cs
+++ yArp/yArp/Form1.Designer.cs
@@ -34,6 +34,13 @@
             this.MAC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SelectAll = new System.Windows.Forms.Button();
             this.DeselectAll = new System.Windows.Forms.Button();
+            this.scan = new System.Windows.Forms.Button();
+            this.SubnetLabel = new System.Windows.Forms.Label();
+            this.subnetTextBox = new System.Windows.Forms.TextBox();
+            this.AutoDetect = new System.Windows.Forms.CheckBox();
+            this.NetworkAdapterLabel = new System.Windows.Forms.Label();
+            this.AdapterList = new System.Windows.Forms.ListBox();
+            this.RefreshAdapters = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Devices
@@ -86,17 +93,92 @@
             this.DeselectAll.UseVisualStyleBackColor = true;
             this.DeselectAll.Click += new System.EventHandler(this.DeselectAll_Click);
             // 
+            // scan
+            // 
+            this.scan.Location = new System.Drawing.Point(415, 286);
+            this.scan.Name = "scan";
+            this.scan.Size = new System.Drawing.Size(139, 23);
+            this.scan.TabIndex = 3;
+            this.scan.Text = "Scan Network";
+            this.scan.UseVisualStyleBackColor = true;
+            this.scan.Click += new System.EventHandler(this.scan_Click);
+            // 
+            // SubnetLabel
+            // 
+            this.SubnetLabel.AutoSize = true;
+            this.SubnetLabel.Location = new System.Drawing.Point(412, 12);
+            this.SubnetLabel.Name = "SubnetLabel";
+            this.SubnetLabel.Size = new System.Drawing.Size(44, 13);
+            this.SubnetLabel.TabIndex = 4;
+            this.SubnetLabel.Text = "Subnet:";
+            // 
+            // subnetTextBox
+            // 
+            this.subnetTextBox.Location = new System.Drawing.Point(415, 28);
+            this.subnetTextBox.Name = "subnetTextBox";
+            this.subnetTextBox.Size = new System.Drawing.Size(139, 20);
+            this.subnetTextBox.TabIndex = 5;
+            // 
+            // AutoDetect
+            // 
+            this.AutoDetect.AutoSize = true;
+            this.AutoDetect.Checked = true;
+            this.AutoDetect.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AutoDetect.Location = new System.Drawing.Point(415, 54);
+            this.AutoDetect.Name = "AutoDetect";
+            this.AutoDetect.Size = new System.Drawing.Size(120, 17);
+            this.AutoDetect.TabIndex = 6;
+            this.AutoDetect.Text = "Auto-Detect Subnet";
+            this.AutoDetect.UseVisualStyleBackColor = true;
+            // 
+            // NetworkAdapterLabel
+            // 
+            this.NetworkAdapterLabel.AutoSize = true;
+            this.NetworkAdapterLabel.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.NetworkAdapterLabel.Location = new System.Drawing.Point(412, 76);
+            this.NetworkAdapterLabel.Name = "NetworkAdapterLabel";
+            this.NetworkAdapterLabel.Size = new System.Drawing.Size(87, 13);
+            this.NetworkAdapterLabel.TabIndex = 7;
+            this.NetworkAdapterLabel.Text = "Network Adapter";
+            // 
+            // AdapterList
+            // 
+            this.AdapterList.FormattingEnabled = true;
+            this.AdapterList.Location = new System.Drawing.Point(415, 93);
+            this.AdapterList.Name = "AdapterList";
+            this.AdapterList.Size = new System.Drawing.Size(120, 69);
+            this.AdapterList.TabIndex = 8;
+            // 
+            // RefreshAdapters
+            // 
+            this.RefreshAdapters.Location = new System.Drawing.Point(415, 170);
+            this.RefreshAdapters.Name = "RefreshAdapters";
+            this.RefreshAdapters.Size = new System.Drawing.Size(120, 23);
+            this.RefreshAdapters.TabIndex = 9;
+            this.RefreshAdapters.Text = "Refresh Adapters";
+            this.RefreshAdapters.UseVisualStyleBackColor = true;
+            this.RefreshAdapters.Click += new System.EventHandler(this.RefreshAdapters_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(583, 424);
+            this.Controls.Add(this.RefreshAdapters);
+            this.Controls.Add(this.AdapterList);
+            this.Controls.Add(this.NetworkAdapterLabel);
+            this.Controls.Add(this.AutoDetect);
+            this.Controls.Add(this.subnetTextBox);
+            this.Controls.Add(this.SubnetLabel);
+            this.Controls.Add(this.scan);
             this.Controls.Add(this.DeselectAll);
             this.Controls.Add(this.SelectAll);
             this.Controls.Add(this.Devices);
             this.Name = "Form1";
             this.Text = "yArp";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,6 +190,13 @@
         private System.Windows.Forms.ColumnHeader MAC;
         private System.Windows.Forms.Button SelectAll;
         private System.Windows.Forms.Button DeselectAll;
+        private System.Windows.Forms.Button scan;
+        private System.Windows.Forms.Label SubnetLabel;
+        private System.Windows.Forms.TextBox subnetTextBox;
+        private System.Windows.Forms.CheckBox AutoDetect;
+        private System.Windows.Forms.Label NetworkAdapterLabel;
+        private System.Windows.Forms.ListBox AdapterList;
+        private System.Windows.Forms.Button RefreshAdapters;
     }
 }
 
--- yArp/yArp/Form1.cs
+++ yArp/yArp/Form1.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,29 +18,30 @@ namespace yArp
         public Form1()
         {
             InitializeComponent();
-            Devices.MouseClick += Devices_MouseClick;      
+            Devices.MouseClick += Devices_MouseClick;
 
             string[] rows = { "test", "test2", "test3" };
             var listViewItem = new ListViewItem(rows);
             Devices.Items.Add(listViewItem);
+
         }
 
         private void Devices_MouseClick(object sender, MouseEventArgs e)
         {
-           
-           foreach(ListViewItem I in Devices.Items)
+
+            foreach (ListViewItem I in Devices.Items)
             {
                 SelectDeselect(I, !I.Checked);
-            } 
+            }
         }
 
         private void Devices_SelectedIndexChanged(object sender, EventArgs e)
-        {   
+        {
         }
 
         private void SelectAll_Click(object sender, EventArgs e)
         {
-            foreach(ListViewItem I in Devices.Items)
+            foreach (ListViewItem I in Devices.Items)
             {
                 I.Checked = true;
                 SelectDeselect(I, true);
@@ -55,7 +59,7 @@ namespace yArp
 
         private void SelectDeselect(ListViewItem I, bool select)
         {
-           
+
             if (select)
             {
                 I.BackColor = SystemColors.MenuHighlight;
@@ -64,6 +68,58 @@ namespace yArp
             {
                 I.BackColor = SystemColors.Window;
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scan_Click(object sender, EventArgs e)
+        {
+            subnetTextBox.Text = getSubnet();
+            //get subnet
+        }
+
+        private string getSubnet()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation ip in nic.GetIPProperties().UnicastAddresses)
+                {
+                    if (nic.Name == AdapterList.SelectedItem.ToString())
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            return FilterToSubnet(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+            return "not found";
+        }
+
+        private string FilterToSubnet(string input)
+        {
+            //example 192.16.0.1 -> 192.16.0.
+            return input.Substring(0, input.LastIndexOf("."));
+        }
+
+        private void GetAdapters()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (!AdapterList.Items.Contains(nic.Name))
+                {
+                    
+                    AdapterList.Items.Add(nic.Name);
+                }
+            }
+        }
+
+        private void RefreshAdapters_Click(object sender, EventArgs e)
+        {
+            GetAdapters();
         }
     }
 }
